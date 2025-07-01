33ede0a3077cec768b61b86f785b86009de53047
neriyabl <neriyabl555@gmail.com>
2025-07-01 23:51:39 +0300

    run watcher tests serially

--- tests/watcher.rs
+++ tests/watcher.rs
@@ -1,11 +1,13 @@
 use std::path::Path;
 
 use hit_with_gpt::watcher::send_change_to_server;
+use serial_test::serial;
 use httpmock::Method::POST;
 use httpmock::MockServer;
 
 
 #[test]
+#[serial]
 fn reports_change_to_server() {
     let server = MockServer::start();
     let partial = serde_json::json!({"hash": "abcd", "path": "foo.txt"}).to_string();
@@ -25,6 +27,7 @@ fn reports_change_to_server() {
 }
 
 #[test]
+#[serial]
 fn error_when_unreachable() {
     unsafe { std::env::set_var("HIT_SERVER_URL", "http://127.0.0.1:59999"); }
     let err = send_change_to_server("abcd", Path::new("foo.txt"));
